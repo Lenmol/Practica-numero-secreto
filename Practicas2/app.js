b124fbc76a54e6bd04ea554e5e16f958f0b5e9e9
Lenmol <158082354+Lenmol@users.noreply.github.com>
2024-04-05 10:57:42 -0300

    ingreso de cantidad de intentos user

--- Practicas2/app.js
+++ Practicas2/app.js
@@ -1,22 +1,22 @@
 const leer = require("prompt-sync")();
 
-const MAX_INTENTOS = 10;
-
 let intentDeseadosUsuar = 0;
-let dobleIntentos = intentDeseadosUsuar * 2;
-let numSecreto = Math.floor(Math.random()*dobleIntentos + 1);
 
 let numIngresado = 0;
-
-
-console.log("Bienvenido a adivinar el numero secreto")
-console.log("Cuantos intentos quiere tener?");
+let numSecreto = 0;
+console.log("Bienvenido a adivinar el numero secreto");
+console.log("ingrese cantidad de intentos")
 intentDeseadosUsuar = Number(leer());
 
+numSecreto =Math.floor(Math.random() * (intentDeseadosUsuar*2) + 1);
 
-for (let i = 1 ; i<=intentDeseadosUsuar ; i++){
+console.log(intentDeseadosUsuar);
+console.log(numSecreto)
+
+for (let i = 1; i <=intentDeseadosUsuar ; i++) {
     console.log("Ingrese un numero");
     numIngresado = Number(leer());
+    
 
     if(numIngresado == numSecreto){
         console.log("Adivinaste el numero, es:",numSecreto);
